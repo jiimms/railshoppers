5737de93d2b45df55748c96d794bb3b7d22165de
jiimms <jiimms@yahoo.com>
2017-02-09 22:32:13 +0530

    modify paperclip missing image

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
 	validates_presence_of :name
-	has_attached_file :avatar, styles: {medium: "300x300>", thumb: "100x100>" }, default_url: "missing_:style.jpg"
+	has_attached_file :avatar, styles: {medium: "300x300>", thumb: "100x100>" }, default_url: "missing.jpg"
   	validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
 end
