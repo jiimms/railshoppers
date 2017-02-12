e9a3cf1238c7282d20f6d720041f081b465aded1
jiimms <jiimms@yahoo.com>
2017-02-12 23:10:29 +0530

    configure aws

--- config/application.rb
+++ config/application.rb
@@ -6,10 +6,20 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module RailsHoppers
+module ImagePaperclip
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-  end
+ 
+
+config.before_configuration do
+  env_file = File.join(Rails.root, 'config', 'aws.yml')
+  YAML.load(File.open(env_file)).each do |key, value|
+    ENV[key.to_s] = value
+  end if File.exists?(env_file)
+end
+
+
+ end
 end
